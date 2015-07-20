270abaf0c57ab785de1f62ca997266270d2108d9
Doug Winter <doug.winter@isotoma.com>
2015-07-20 13:53:51 +0100

    kind of works

--- simple.py
+++ simple.py
@@ -1,14 +1,16 @@
 from twisted.internet import reactor
+from twisted.internet.defer import Deferred
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 from twisted.internet.ssl import ClientContextFactory
+from twisted.internet.protocol import Protocol
 from base64 import b64encode
+import json
 
 password = open("token").read().strip()
 username = 'winjer'
 
-authorization = b64encode("%s:%s" % (username, password)).decode("ascii")
-print authorization
+authorization = "Basic " + str(b64encode("%s:%s" % (username, password)).decode("ascii"))
 
 class WebClientContextFactory(ClientContextFactory):
     def getContext(self, hostname, port):
@@ -23,18 +25,32 @@ d = agent.request(
     'https://api.github.com/users/winjer/orgs',
     Headers({
         'Accept': ['application/vnd.github.v3+json'],
-        'Authorization': ["Basic d2luamVyOjViMjM5MGZmMDExZmQzMjg1OWYxOGVjNDcyYTJiMWI1YzJjMjEzNmI="],
+        'Authorization': [authorization],
         'User-Agent': ['htmlhub'],
     }),
     None
 )
 
+class Consumer(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.data = []
+
+    def dataReceived(self, bytes):
+        self.data.append(bytes)
+
+    def connectionLost(self, reason):
+        self.finished.callback(None)
+        print json.loads("".join(self.data))
+
 def cbResponse(response):
-    print response.__dict__
+    finished = Deferred()
+    response.deliverBody(Consumer(finished))
+    return finished
+
 d.addCallback(cbResponse)
 
 def cbError(response):
-    print response
     print response.__dict__
 d.addErrback(cbError)
 
